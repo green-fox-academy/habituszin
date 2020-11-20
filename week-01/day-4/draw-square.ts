458fc91d45594843006e757c72b73f0d119f6969
Habituszin <novakmark19@gmail.com>
2020-11-20 12:44:00 +0100

    finished the square

---
+++ week-01/day-4/draw-square.ts
@@ -0,0 +1,32 @@
+'use strict';
+
+let count: number = 6;
+
+// Write a program that draws a square like this:
+//
+// %%%%%%
+// %    %
+// %    %
+// %    %
+// %    %
+// %%%%%%
+//
+// The square should have as many lines as lineCount is
+for (let i = 0; i < count; i++) {
+    let line: string = '';
+    if (i == 0 || i == (count - 1)) {
+        for (let j = 0; j < count; j++) {
+            line += '%';
+        }
+        console.log(line);
+
+    } else if (i > 0 && i < (count - 1)) {
+        line = '%';
+        for (let k = 0; k < count - 2; k++) {
+            line += ' ';
+        }
+        line += '%';
+        console.log(line);
+    }
+}
+
