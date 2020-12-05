f20277fab595b5176376b6044ccc4f5bd349783a
Habituszin <novakmark19@gmail.com>
2020-12-05 12:09:44 +0100

    finaly finished the fractal circle

---
+++ week-03/day-5/circle/circle.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas');
+const ctx = canvas.getContext('2d');
+
+function drawCircle(x, y, r) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.stroke();
+}
+
+function drawFraclatCircles(beginingX, beginingY, radius) {
+  let height = (radius / 2)  * Math.sin(Math.PI / 4);
+  if (radius < 10) {
+    return
+  }
+  drawCircle(beginingX, beginingY, radius);
+  drawFraclatCircles(beginingX, beginingY - radius /2, radius / 2);
+  drawFraclatCircles(beginingX + height, beginingY + height, radius / 2);
+  drawFraclatCircles(beginingX - height, beginingY + height, radius / 2);
+
+}
+
+drawFraclatCircles(300,200,200);
