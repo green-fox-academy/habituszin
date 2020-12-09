60b54e38521bb587ac1f3642e6f0cbb46db30278
Habituszin <novakmark19@gmail.com>
2020-12-09 17:24:09 +0100

    sharpie tested

---
+++ week-04/day-3/sharpie/sharpie.test.ts
@@ -0,0 +1,34 @@
+'use strict';
+
+import * as test from "tape";
+import { Sharpie } from "../../../week-03/day-3/sharpie/sharpie";
+
+test('test get color', t => {
+  let sharpie = new Sharpie('yellow', 5);
+
+  let actual = sharpie.color;
+  let extend = 'yellow';
+
+  t.equal(actual, extend);
+  t.end();
+})
+
+test('test inklevel', t => {
+  let sharpie = new Sharpie('yellow', 5);
+
+  let actual = sharpie.inklevel;
+  let extend = 100;
+
+  t.equal(actual, extend);
+  t.end();
+})
+
+test('test use', t => {
+  let sharpie = new Sharpie('yellow', 5);
+  sharpie.use(80);
+  let actual = sharpie.inklevel;
+  let extend = 20;
+
+  t.equal(actual, extend);
+  t.end();
+})
