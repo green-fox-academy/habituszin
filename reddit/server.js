0df1cd4f32a4dc823ed4a82fae0431aebf42df6d
Habituszin <novakmark19@gmail.com>
2021-01-15 18:03:49 +0100

    make the server and database for reddit

---
+++ reddit/server.js
@@ -0,0 +1,53 @@
+'use stirct';
+
+const { json } = require('express');
+const express = require('express');
+const app = express();
+const port = 3000;
+const mysql = require('mysql');
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+let conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'reddit',
+});
+
+
+conn.connect((err) => {
+  if (err) {
+    console.error('Cannot connect to the database', err);
+    return;
+  }
+  console.log('Connection established');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/main.html')
+});
+
+/* app.get('/posts', (req, res) => {
+  let user = req.body.user;
+  res.status(200).json(user)
+}); */
+
+app.get('/posts', (req, res) => {
+  conn.query(`SELECT posts.id, title, url, date, vote_numbers, name, vote 
+  FROM posts 
+  INNER JOIN users ON posts.creator_id = users.id 
+  INNER JOIN votes ON posts.id = votes.post_id 
+  WHERE votes.user_id = (?);`, [req.body.user], (err, rows) => {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).json({ 'error': 'database error' });
+      return;
+    }
+    console.log(rows);
+    res.status(200).json(rows);
+  });
+});
+
+app.listen(port);
