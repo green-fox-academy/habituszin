5b6b99e913edf86460317441bf86b4da74eb75cf
Habituszin <novakmark19@gmail.com>
2021-01-25 14:19:03 +0100

    add error handing

--- week-09/day-1/promise-it-wont-hurt/promise/main.js
+++ week-09/day-1/promise-it-wont-hurt/promise/main.js
@@ -5,16 +5,21 @@ let button = document.querySelector('button');
 button.addEventListener('click', () => {
   fetch('https://api.chucknorris.io/jokes/random', {
     method: 'GET',
-    headers: {'Content-Type': 'application/json'},
-})
-.then(response => response.json())
-.then(responseObjecttoJson => {     
-  let newElement = document.createElement('h3');
-  newElement.innerHTML = responseObjecttoJson.value;
-  container.appendChild(newElement);
-})
-.catch(err => {
-    console.log('valami baj van');
-})
-
+    headers: { 'Content-Type': 'application/json' },
+  })
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error('nincs valasz')
+      }
+      return response;
+    })
+    .then(response => response.json())
+    .then(responseObjecttoJson => {
+      let newElement = document.createElement('h3');
+      newElement.innerHTML = responseObjecttoJson.value;
+      container.appendChild(newElement);
+    })
+    .catch(err => {
+      console.log('valami baj van');
+    })
 })
