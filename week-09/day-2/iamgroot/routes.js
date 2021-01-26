80c2a4828b2b566ca81814dda8f8dfd6e5d4a57d
Habituszin <novakmark19@gmail.com>
2021-01-26 17:38:41 +0100

    i cant believe but it is done

--- week-09/day-2/iamgroot/routes.js
+++ week-09/day-2/iamgroot/routes.js
@@ -3,12 +3,73 @@ const app = express();
 
 let iAmGroot = 'I am Groot!';
 
+let cargo = {
+  caliber25: 0,
+  caliber30: 0,
+  caliber50: 0,
+  shipstatus: 'empty',
+  ready: false
+};
+
+function shipChecker(){
+  let totalBullet = cargo.caliber25 + cargo.caliber30 + cargo.caliber50;
+
+  if (totalBullet == 0) {
+    cargo.ready = false
+    cargo.shipstatus = 'empty'
+  } else if (totalBullet > 0 && totalBullet < 12500) {
+    cargo.ready = false
+    cargo.shipstatus = `${Math.floor(totalBullet / 12500 * 100)}%`
+  } else if (totalBullet > 12500 ) {
+    cargo.ready = true
+    cargo.shipstatus = 'overloaded'
+  } else if (totalBullet == 12500) {
+    cargo.shipstatus = '100%'
+    cargo.ready = true
+  }  
+}
+
 app.get('/groot', (req, res) => {
   let message = req.query.message;
   if (!message || message == '') {
-    res.status(400).json({'error': iAmGroot})
+    res.status(400).json({ 'error': iAmGroot })
+    return
   }
-  res.status(200).json({'message': message, 'translate': iAmGroot})
+  res.status(200).json({ 'message': message, 'translate': iAmGroot })
 });
+
+app.get('/yondu', (req, res) => {
+  let distance = req.query.distance;
+  let time = req.query.time;
+  let speed = distance / time;
+  if (time <= 0) {
+    res.status(400).json({ 'error': 'teleport' })
+    return
+  }
+  if (!distance || distance == NaN || !time || time == NaN) {
+    res.status(400).json({ 'error': 'PLsss, be pretty' })
+    return
+  }
+  res.status(200).json({ "distance": distance, "time": time, "speed": speed })
+})
+
+app.get('/rocket', (req, res) => {
+  res.status(200).json(cargo)
+})
+
+app.get('/rocket/fill', (req, res) => {
+  let caliber = 'caliber' + req.query.caliber.split('.')[1];
+  let amount = Number(req.query.amount);
+
+  Object.keys(cargo).forEach((element) => {
+    if (element == caliber) {
+      cargo[caliber] += amount 
+    }
+  })
+
+  shipChecker();
+  res.status(200).json({"caliber": req.query.caliber, "amount": amount, "shipstatus": cargo.shipstatus, "ready": cargo.ready})
+})
+
 
 module.exports = app;
--- week-09/day-2/iamgroot/test/test-routes.js
+++ week-09/day-2/iamgroot/test/test-routes.js
@@ -7,21 +7,71 @@ const app = require('../routes');
 describe('groot endpont', () => {
   it('GET /groot should return transfer', () => {
     request(app)
-    .get('/groot')
-    .query({message: 'something'})
-    .expect(200)
-    .end((err,res) => {
-      expect(err).to.be.null;
-      expect(res.body.translate).to.equal('I am Groot!');
-    })
+      .get('/groot')
+      .query({ message: 'something' })
+      .expect(200)
+      .end((err, res) => {
+        expect(res.body.translate).to.equal('I am Groot!');
+      })
   })
   it('GET /groot should return error', () => {
     request(app)
-    .get('/groot')
-    .expect(400)
-    .end((err,res) => {
-      expect(err).to.be.null;
-      expect(res.body.error).to.equal('I am Groot!')
-    })
+      .get('/groot')
+      .expect(400)
+      .end((err, res) => {
+        expect(res.body.error).to.equal('I am Groot!')
+      })
   })
+})
+
+describe('yondu endpoint', () => {
+  it('GET /yondu should return object with speed', () => {
+    request(app)
+      .get('/yondu')
+      .query({ distance: 100, time: 10 })
+      .expect(200)
+      .end((err, res) => {
+        expect(res.body.speed).to.equal(10);
+      })
+  })
+  it('GET /yondu should return error teleport', () => {
+    request(app)
+      .get('/yondu')
+      .query({ distance: 100, time: 0 })
+      .expect(400)
+      .end((err, res) => {
+        expect(res.body.error).to.equal('teleport');
+      })
+  })
+  it('GET /yondu should return error ', () => {
+    request(app)
+      .get('/yondu')
+      .expect(400)
+      .end((err, res) => {
+        expect(res.body.error).to.equal('PLsss, be pretty');
+      })
+  })
+})
+
+describe('rocket endpont', () => {
+  it('GET /rocket should return cargo database', () => {
+    request(app)
+      .get('/rocket')
+      .expect(200)
+      .end((err, res) => {
+        expect(Object.keys(res.body).length).to.equal(5)
+      })
+  })
+})
+
+describe('rocket fill endpont', () =>{
+  it('get /rocket/fill return bullet number', () => {
+    request(app)
+    .get('/rocket/fill')
+    .query({caliber: '.50', amount: 5000})
+    .expect(200)
+    .end((err, res) => {
+      expect(res.body).to.deep.equal({caliber: '.50', amount: 5000, shipstatus: '40%', ready: false})
+    })
+  }) 
 })
